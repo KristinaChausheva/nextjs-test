e538ccd9e25ca3ac89c1ebd6d403a8ac4ad6ca0a
KristinaChausheva <52411816+KristinaChausheva@users.noreply.github.com>
2022-03-26 22:25:50 +0000

    Deploying to gh-pages from @ KristinaChausheva/nextjs-test@595d33279e25bc45828288c5d65469b4027809a3 🚀

--- _next/static/a-sbSOTeL46YCwrJ3_a8c/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-6d9ae06e5a07a9fa.js","static/css/a43d2365bb8a381a.css","static/chunks/pages/index-f1116b982ff87d08.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
