7382d26b17c36bf16389e7361ae71ee6598b793a
Kristina Chausheva <k.chausheva@gmail.com>
2022-03-29 00:55:18 +0300

    form and thank you pages

---
+++ components/Form.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+function Form() {
+  return (
+    <form action="https://formsubmit.co/k.chausheva@gmail.com" method="POST">
+      <input
+        type="text"
+        name="message"
+        placeholder="write your message here"
+        required
+      />
+      <input type="email" name="email" placeholder="email" required />
+      <input type="hidden" name="_subject" value="New email!"></input>
+      <input
+        type="hidden"
+        name="_next"
+        value="https://kristinachausheva.github.io/nextjs-test/thanks.html"
+      />
+      <button type="submit">Send</button>
+    </form>
+  )
+}
+
+export default Form
---
+++ pages/form.js
@@ -0,0 +1,9 @@
+import Form from "../components/Form"
+
+import React from "react"
+
+function contactForm() {
+  return <Form />
+}
+
+export default contactForm
