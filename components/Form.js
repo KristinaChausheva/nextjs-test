eb613dfcc37fd6d35004bc36f74725559e363de9
Kristina Chausheva <k.chausheva@gmail.com>
2022-04-01 00:35:50 +0300

    form styling

--- components/Form.js
+++ components/Form.js
@@ -2,34 +2,13 @@ import React from "react"
 
 function Form() {
   return (
-    //   <div class="login-box">
-    //   <h2>Login</h2>
-    //   <form>
-    //     <div class="user-box">
-    //       <input type="text" name="" required="" />
-    //       <label>Username</label>
-    //     </div>
-    //     <div class="user-box">
-    //       <input type="password" name="" required="" />
-    //       <label>Password</label>
-    //     </div>
-    //     <a href="#">
-    //       <span></span>
-    //       <span></span>
-    //       <span></span>
-    //       <span></span>
-    //       Submit
-    //     </a>
-    //   </form>
-    // </div>
-
-    <form
-      action="https://formsubmit.co/k.chausheva@gmail.com"
-      method="POST"
-      className="form"
-    >
-      <div className="box">
-        <h2>Send message</h2>
+    <div className="box">
+      <h2>Send message</h2>
+      <form
+        action="https://formsubmit.co/k.chausheva@gmail.com"
+        method="POST"
+        className="form"
+      >
         <div className="user-box">
           <input className="formInput" type="email" name="email" required />
           <label>email</label>
@@ -44,25 +23,29 @@ function Form() {
           />
           <label>write your message here</label>
         </div>
-      </div>
 
-      <input
-        className="formInput"
-        type="hidden"
-        name="_subject"
-        value="New email!"
-      ></input>
-      <input
-        className="formInput"
-        type="hidden"
-        name="_next"
-        value="https://kristinachausheva.github.io/nextjs-test/thanks.html"
-      />
-      <input type="hidden" name="_captcha" value="false"></input>
-      <button className="formButton" type="submit">
-        Send
-      </button>
-    </form>
+        <input
+          className="formInput"
+          type="hidden"
+          name="_subject"
+          value="New email!"
+        ></input>
+        <input
+          className="formInput"
+          type="hidden"
+          name="_next"
+          value="https://kristinachausheva.github.io/nextjs-test/thanks.html"
+        />
+        <input type="hidden" name="_captcha" value="false"></input>
+        <button className="formButton" type="submit">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Send
+        </button>
+      </form>
+    </div>
   )
 }
 
