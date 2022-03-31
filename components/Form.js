f3483a0bcc0373c1c147b033b54619ebb398c654
Kristina Chausheva <k.chausheva@gmail.com>
2022-03-31 23:45:55 +0300

    adding classes

--- components/Form.js
+++ components/Form.js
@@ -2,22 +2,41 @@ import React from "react"
 
 function Form() {
   return (
-    <form action="https://formsubmit.co/k.chausheva@gmail.com" method="POST">
+    <form
+      action="https://formsubmit.co/k.chausheva@gmail.com"
+      method="POST"
+      className="form"
+    >
       <input
+        className="formInput"
         type="text"
         name="message"
         placeholder="write your message here"
         required
       />
-      <input type="email" name="email" placeholder="email" required />
-      <input type="hidden" name="_subject" value="New email!"></input>
       <input
+        className="formInput"
+        type="email"
+        name="email"
+        placeholder="email"
+        required
+      />
+      <input
+        className="formInput"
+        type="hidden"
+        name="_subject"
+        value="New email!"
+      ></input>
+      <input
+        className="formInput"
         type="hidden"
         name="_next"
         value="https://kristinachausheva.github.io/nextjs-test/thanks.html"
       />
       <input type="hidden" name="_captcha" value="false"></input>
-      <button type="submit">Send</button>
+      <button className="formButton" type="submit">
+        Send
+      </button>
     </form>
   )
 }
