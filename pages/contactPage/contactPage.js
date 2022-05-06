e0728ec8b3f6945552553458028acf5bdae1432c
Kristina Chausheva <k.chausheva@gmail.com>
2022-05-06 22:05:31 +0300

    cleaning

--- pages/contactPage/contactPage.js
+++
@@ -1,9 +0,0 @@
-import Form from "../../components/Form/Form"
-
-import React from "react"
-
-function form() {
-  return <Form />
-}
-
-export default form
--- pages/mySkills/mySkills.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-function mySkills() {
-  return <div>my skills</div>
-}
-
-export default mySkills
